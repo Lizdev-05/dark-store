fc18fe8b48ae32c377881e169d65a79b03a0812e
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-05-17 19:12:01 +0100

    set color scheme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Footer from "./component/Footer/Footer";
 import Nav from "./component/Header/Nav";
-import Hero from "./component/Page/Hero";
+import Hero from "./component/hero/Hero";
 
 function App() {
   return (
