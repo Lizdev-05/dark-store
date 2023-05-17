ed44f26a10feb49e24b2000b59728ab04454428f
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-05-17 21:59:16 +0100

    Style the hero

--- src/component/hero/Hero.js
+++ src/component/hero/Hero.js
@@ -1,26 +1,27 @@
 import React from "react";
 import phone from "../../assets/phone.svg";
+import "./Hero.css";
 
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="container --flex-between">
+      <div className="container --grid-15">
         <div className="hero-content">
           <h1>Visit Lizdev Landing Page</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
-            iusto recusandae soluta cupiditate quasi amet accusamus nemo dolor
-            suscipit illo ratione in provident accusantium sit repellat
-            exercitationem, obcaecati veniam corporis corrupti cum, deserunt
-            dolorem excepturi praesentium architecto. Minus, quibusdam quasi
-            iusto distinctio, nesciunt eius facilis soluta rerum unde totam ut.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo animi
+            eveniet repellendus quidem qui enim, porro commodi fugiat odit nobis
+            sit optio consectetur excepturi ad minus dignissimos alias dicta
+            laboriosam expedita quam magni eligendi ut adipisci. Voluptatem
+            eaque unde quisquam assumenda veniam natus dolore illo officia,
+            magni quia ad. Hic.
           </p>
-          <div className="btn">
-            <button>Learn More</button>
-            <button>Sign Up</button>
+          <div className="--flex-start">
+            <button className="--btn btn-p">Learn More</button>
+            <button className="--btn --btn-danger">Sign Up</button>
           </div>
         </div>
-        <div className="hero-image">
+        <div className="--text-center">
           <img src={phone} alt="phone image" width={200} />
         </div>
       </div>
