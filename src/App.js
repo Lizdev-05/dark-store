e06789838248fb38d58bc807ced2761af4177bc8
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-05-17 17:44:45 +0100

    Create the pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Footer from "./component/Footer/Footer";
+import Nav from "./component/Header/Nav";
+import Hero from "./component/Page/Hero";
 
 function App() {
-  return <div className="App">This is the app page</div>;
+  return (
+    <div className="App">
+      This is the app page
+      <Nav />
+      <Hero />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
