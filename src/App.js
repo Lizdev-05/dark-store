34fc632d74f2a61ca3c4b5a2d372353cbff1ac05
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-05-25 10:46:54 +0100

    Create function for setting of theme

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import "./App.css";
 import Footer from "./component/Footer/Footer";
 import Nav from "./component/Header/Nav";
 import Hero from "./component/hero/Hero";
+import useLocalStorage from "use-local-storage";
 
 function App() {
+  const [theme, setTheme] = useLocalStorage("", "light");
+
+  const newThemee = theme === "light" ? "dark" : "light";
+  setTheme(newThemee);
   return (
     <div className="main-app">
       <Nav />
