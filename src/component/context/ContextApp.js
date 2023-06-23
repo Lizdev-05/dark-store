c2e0b55dd3aeae0185d973d495db01b8014b5eda
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-23 22:31:07 +0100

    use context provider custom

--- src/component/context/ContextApp.js
+++ src/component/context/ContextApp.js
@@ -2,32 +2,18 @@ import { useEffect, useState } from "react";
 import Footer from "./Footer";
 import Nav from "./Nav";
 import Hero from "./Hero";
-import useLocalStorage from "use-local-storage";
-import LightModeContext from "../../contextApi/themeContext";
+
+import { ThemeContextProvider } from "../../contextApi/themeContext";
 
 function ContextApp() {
-  const [theme, setTheme] = useLocalStorage("theme", "light");
-
-  const [switchBtn, setSwitchBtn] = useState(false);
-
-  const toggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-    setSwitchBtn(!switchBtn);
-  };
-
-  useEffect(() => {
-    if (theme === "dark") setSwitchBtn(true);
-  }, []);
-
   return (
-    <LightModeContext.Provider value={{ theme, toggleTheme, switchBtn }}>
+    <ThemeContextProvider>
       <div className="main-app">
         <Nav />
         <Hero />
         <Footer />
       </div>
-    </LightModeContext.Provider>
+    </ThemeContextProvider>
   );
 }
 
--- src/contextApi/themeContext.js
+++ src/contextApi/themeContext.js
@@ -1,5 +1,32 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import useLocalStorage from "use-local-storage";
 
 const ThemeContext = createContext();
+
+export const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useLocalStorage("theme", "light");
+
+  const [switchBtn, setSwitchBtn] = useState(false);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    setSwitchBtn(!switchBtn);
+  };
+
+  useEffect(() => {
+    if (theme === "dark") setSwitchBtn(true);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme, switchBtn }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// export const useThemeContext = () => {
+//   return useContext(ThemeContext);
+// };
 
 export default ThemeContext;
